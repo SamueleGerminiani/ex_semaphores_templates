63f2beb653d313eb225b6eb52b9757e5a29bea3b
Samuele Germiniani <samuele.germiniani@univr.it>
2024-11-28 17:13:56 +0100

    changed semOp structure hint

--- ese_2/src/semaphore.c
+++ ese_2/src/semaphore.c
@@ -4,8 +4,11 @@
 #include "errExit.h"
 
 void semOp (int semid, unsigned short sem_num, short sem_op) {
-    struct sembuf sop = { /*...*/};
+    struct sembuf sop;
+    //Populate the sembuf structure
+    //for example: sop.sem_num = sem_num;
 
+    //run the semop system call
     if (semop(/*...*/) == /*...*/)
         errExit("semop failed");
 }
